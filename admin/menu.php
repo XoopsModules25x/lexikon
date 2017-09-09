6cf78710c796c58921b2a3d5805150f397355fbd
mambax7 <mambax7@gmail.com>
2017-09-09 11:07:37 -0400

    Menu standardization

--- admin/menu.php
+++ admin/menu.php
@@ -12,44 +12,57 @@ if (false !== ($moduleHelper = Xmf\Module\Helper::getHelper($moduleDirName))) {
     $moduleHelper = Xmf\Module\Helper::getHelper('system');
 }
 
-
 $pathIcon32 = \Xmf\Module\Admin::menuIconPath('');
 //$pathModIcon32 = $moduleHelper->getModule()->getInfo('modicons32');
 
 $moduleHelper->loadLanguage('modinfo');
 
-$adminmenu              = [];
-$i                      = 0;
-'title' =>  _AM_MODULEADMIN_HOME,
-'link' =>  'admin/index.php',
-'icon' =>  $pathIcon32 . '/home.png',
-++$i;
-'title' =>  _MI_LEXIKON_ADMENU1,
-'link' =>  'admin/main.php',
-'icon' =>  $pathIcon32 . '/manage.png',
-++$i;
-'title' =>  _MI_LEXIKON_ADMENU2,
-'link' =>  'admin/category.php',
-'icon' =>  $pathIcon32 . '/category.png',
-++$i;
-'title' =>  _MI_LEXIKON_ADMENU3,
-'link' =>  'admin/entry.php',
-'icon' =>  $pathIcon32 . '/add.png',
-++$i;
-'title' =>  _MI_LEXIKON_ADMENU12,
-'link' =>  'admin/statistics.php',
-'icon' =>  $pathIcon32 . '/stats.png',
-++$i;
-'title' =>  _MI_LEXIKON_ADMENU9,
-'link' =>  'admin/permissions.php',
-'icon' =>  $pathIcon32 . '/permissions.png',
+$adminmenu[] = [
+    'title' => _AM_MODULEADMIN_HOME,
+    'link'  => 'admin/index.php',
+    'icon'  => $pathIcon32 . '/home.png',
+];
 
-++$i;
-'title' =>  _MI_LEXIKON_IMPORT,
-'link' =>  'admin/importwordbook.php',
-'icon' =>  $pathIcon32 . '/compfile.png',
+$adminmenu[] = [
+    'title' => _MI_LEXIKON_ADMENU1,
+    'link'  => 'admin/main.php',
+    'icon'  => $pathIcon32 . '/manage.png',
+];
 
-++$i;
-'title' =>  _AM_MODULEADMIN_ABOUT,
-'link' =>  'admin/about.php',
-'icon' =>  $pathIcon32 . '/about.png',
+$adminmenu[] = [
+    'title' => _MI_LEXIKON_ADMENU2,
+    'link'  => 'admin/category.php',
+    'icon'  => $pathIcon32 . '/category.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_LEXIKON_ADMENU3,
+    'link'  => 'admin/entry.php',
+    'icon'  => $pathIcon32 . '/add.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_LEXIKON_ADMENU12,
+    'link'  => 'admin/statistics.php',
+    'icon'  => $pathIcon32 . '/stats.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_LEXIKON_ADMENU9,
+    'link'  => 'admin/permissions.php',
+    'icon'  => $pathIcon32 . '/permissions.png',
+
+];
+
+$adminmenu[] = [
+    'title' => _MI_LEXIKON_IMPORT,
+    'link'  => 'admin/importwordbook.php',
+    'icon'  => $pathIcon32 . '/compfile.png',
+
+];
+
+$adminmenu[] = [
+    'title' => _AM_MODULEADMIN_ABOUT,
+    'link'  => 'admin/about.php',
+    'icon'  => $pathIcon32 . '/about.png',
+];
