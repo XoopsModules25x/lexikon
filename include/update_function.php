4f4e1b3f4189c756454538ddc4352110b3fff8cb
mambax7 <mambax7@gmail.com>
2021-07-13 23:56:05 -0400

    'mkdir(...)' race condition

--- include/update_function.php
+++ include/update_function.php
@@ -76,21 +76,27 @@ function xoops_module_update_lexikon_v152($xoopsModule)
     // Making of uploads/lexikon folder
     $p_lexikon = XOOPS_UPLOAD_PATH . '/lexikon';
     if (!is_dir($p_lexikon)) {
-        mkdir($p_lexikon, 0777);
+        if (!mkdir($p_lexikon, 0777) && !is_dir($p_lexikon)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $p_lexikon));
+        }
         chmod($p_lexikon, 0777);
     }
     copy($indexFile, $p_lexikon . '/index.html');
     // Making of categories folder
     $pl_categories = $p_lexikon . '/categories';
     if (!is_dir($pl_categories)) {
-        mkdir($pl_categories, 0777);
+        if (!mkdir($pl_categories, 0777) && !is_dir($pl_categories)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $pl_categories));
+        }
         chmod($pl_categories, 0777);
     }
     copy($indexFile, $pl_categories . '/index.html');
 
     $plc_images = $pl_categories . '/images';
     if (!is_dir($plc_images)) {
-        mkdir($plc_images, 0777);
+        if (!mkdir($plc_images, 0777) && !is_dir($plc_images)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $plc_images));
+        }
         chmod($plc_images, 0777);
     }
     copy($indexFile, $plc_images . '/index.html');
@@ -98,14 +104,18 @@ function xoops_module_update_lexikon_v152($xoopsModule)
     // Making of entries folder
     $pl_entries = $p_lexikon . '/entries';
     if (!is_dir($pl_entries)) {
-        mkdir($pl_entries, 0777);
+        if (!mkdir($pl_entries, 0777) && !is_dir($pl_entries)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $pl_entries));
+        }
         chmod($pl_entries, 0777);
     }
     copy($indexFile, $pl_entries . '/index.html');
 
     $ple_images = $pl_entries . '/images';
     if (!is_dir($ple_images)) {
-        mkdir($ple_images, 0777);
+        if (!mkdir($ple_images, 0777) && !is_dir($ple_images)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $ple_images));
+        }
         chmod($ple_images, 0777);
     }
     copy($indexFile, $ple_images . '/index.html');
